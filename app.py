62d01e0ea6ac14fa0e1c6975092497e24a7dc39e
Nova Jack <121238158+nova-jack@users.noreply.github.com>
2024-01-11 11:54:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    # Run the app on localhost (0.0.0.0 means all available network interfaces)
-    # and listen on port 5000
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
