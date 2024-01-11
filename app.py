2c8e7a13891a4be9300a214401a8c894ca885f1b
Nova Jack <121238158+nova-jack@users.noreply.github.com>
2024-01-11 16:25:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
+    # Run the Flask app on port 5000
     app.run(host='0.0.0.0', port=5000)
